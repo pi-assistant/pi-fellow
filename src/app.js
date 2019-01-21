6c6b0bf11847eeaacee6de7649c88ca3a04cec03
hingham <hannah.c.ingham@gmail.com>
2019-01-21 12:44:49 -0800

    adding google-cloud-stuff

--- src/app.js
+++ src/app.js
@@ -3,15 +3,50 @@
 // 3rd Party Resources
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
+// const morgan = require('morgan');
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
 
+const client = new speech.SpeechClient();
+
+const audioFile = './assets/hey-test.flac';
+
+const file = fs.readFileSync(audioFile);
+const audioBytes = file.toString('base64');
+
+const audio = {
+  content: audioBytes,
+};
+
+const config = {
+  encoding: 'FLAC',
+  SampleRateHertz:48000,
+  languageCode: 'en-US',
+};
+
+const request = {
+  audio: audio,
+  config: config,
+};
+
+client 
+  .recognize(request)
+  .then(data =>{
+    const response = data[0];
+    const transcription = response.results
+      .map(result=>result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`transcript: ${transcription}`);
+
+  })
+  .catch(err => console.error('error', err));
 
 // Prepare the express app
 const app = express();
 
 // App Level MW
 app.use(cors());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
