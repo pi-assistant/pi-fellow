9ed795e694756eda1d5cbb28fc477543e8d285e8
hingham <hannah.c.ingham@gmail.com>
2019-01-21 07:50:51 -0800

    server up and running, node module installed

---
+++ src/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+let isRunning = false;
+
+module.exports = {
+  server: app,
+  start: (port) => {
+    if( ! isRunning ) {
+      app.listen(port, () => {
+        isRunning = true;
+        console.log(`Server Up on ${port}`);
+      });
+    }
+    else {
+      console.log('Server is already running');
+    }
+  },
+};
